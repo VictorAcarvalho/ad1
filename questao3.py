a5478835ce0a30d87c710dde5c5dcebb4f66cd6d
Victor Carvalho <carvalho.victorfa@gmail.com>
2022-08-27 18:07:49 -0300

    feat:implementa checagem palindromo

--- questao3.py
+++ questao3.py
@@ -1,5 +1,3 @@
-import string
-
 
 print("="*100)
 print("Bem vindo ao programa verificador de frases")
@@ -8,26 +6,34 @@ def main():
     print("Digite as linhas abaixo")
     line=input()
     splitedLine = line.split(" ")
+    palindromeList= []
     phrases =[]
-    splitedStrings= [] 
+    spplitedPhrases= [] 
     vowels ="AaEeIiOoUu"
     if line =='':
         return print("Nenhuma linha não vazia foi lida!!")
     while line != "":
         phrases.append(line)
-        splitedStrings.append(splitedLine)
+        spplitedPhrases.append(splitedLine)
         line=input()
-        splitedLine = line.split(" ")        
+        splitedLine = line.split(" ") 
+        for i in line:
+            if i[::-1] == i:
+                palindromeList.append(line)
+    palindrome = palindromeList[0]
     mostAccentueLine=mostAccentue(phrases) 
     mostVowel = vowelsCount(mostAccentueLine , vowels)
-    bigWord = biggestWord(splitedStrings)
+    bigWord = biggestWord(spplitedPhrases)
+    palindromeQuantity = palindromeCount(palindrome)
     print("A linha com mais vogais sem acento: {}".format(mostAccentueLine))
     print("Quantidade de vogais sem acento: {}".format(mostVowel))
     print("A maior palavra digita é :  {}".format(bigWord))
-    palindrome(splitedStrings)
-
+    print("Linha com mais Palíndromos: {}".format(palindrome))
+    print("O número de palavras palíndromas:{}".format(palindromeQuantity))
+    
+    
 def mostAccentue(phrasesList):
-    mostAccentuesPhrase = (max(phrasesList, key=str.isascii))
+    mostAccentuesPhrase = (max(phrasesList, key=str.isascii)) 
 
     return mostAccentuesPhrase
 
@@ -41,18 +47,11 @@ def biggestWord(lst):
     for i in lst:
         for j in i:
             mergedList.append(j)
-    resulted = max(mergedList,key=len)
+    resulted = max(mergedList,key=len) 
     return resulted
 
-def palindrome(lst):
-    palindrome= ""
-    for i in lst :
-        for j in i:
-            if j == j[::-1]:
-                palindrome = i
-    return print(palindrome)
-
-    
+def palindromeCount(string:str):
+    splited = len(string.split(" "))-1
+    return splited
 main()
-
 
