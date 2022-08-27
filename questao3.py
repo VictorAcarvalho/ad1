904a80f07fd70c7a88fd9cd19d0d82f702cdbe67
Victor Carvalho <carvalho.victorfa@gmail.com>
2022-08-27 02:31:37 -0300

    implementa até o palindromo

--- questao3.py
+++ questao3.py
@@ -1,17 +1,57 @@
+import string
+
+
 print("="*100)
 print("Bem vindo ao programa verificador de frases")
 
-def 
-
 def main():
     print("Digite as linhas abaixo")
+    line=input()
+    splitedLine = line.split(" ")
     phrases =[]
-    line=input()  
+    splitedStrings= [] 
+    vowels ="AaEeIiOoUu"
     if line =='':
         return print("Nenhuma linha não vazia foi lida!!")
     while line != "":
         phrases.append(line)
+        splitedStrings.append(splitedLine)
         line=input()
-    return phrases
+        splitedLine = line.split(" ")        
+    mostAccentueLine=mostAccentue(phrases) 
+    mostVowel = vowelsCount(mostAccentueLine , vowels)
+    bigWord = biggestWord(splitedStrings)
+    print("A linha com mais vogais sem acento: {}".format(mostAccentueLine))
+    print("Quantidade de vogais sem acento: {}".format(mostVowel))
+    print("A maior palavra digita é :  {}".format(bigWord))
+    palindrome(phrases)
 
+def mostAccentue(phrasesList):
+    mostAccentuesPhrase = (max(phrasesList, key=str.isascii))
+
+    return mostAccentuesPhrase
+
+def vowelsCount(phrase, vowels):
+    phraseVowel = [each for each in phrase if each in vowels]
+    return len(phraseVowel)
+
+
+def biggestWord(lst):
+    mergedList = []
+    for i in lst:
+        for j in i:
+            mergedList.append(j)
+    resulted = max(mergedList,key=len)
+    return resulted
+
+def palindrome(lst):
+    palindrome= []
+    for i in lst :
+        if i == i[::-1] :
+            palindrome.append(i)
+    return print(palindrome)
+
+    
 main()
+
+
